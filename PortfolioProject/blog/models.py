f926f7545fedbb05468634bbda2f7588cc2c2c5c
Silas Jimmy <jimmysilas17@gmail.com>
2021-03-27 17:34:14 +0300

    Blog app up and running

--- PortfolioProject/blog/models.py
+++ PortfolioProject/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    """
+    Blog model object.
+    """
+
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    # def __init__(self, arg):
+    #     super(Blog, self).__init__()
+    #     self.arg = arg
