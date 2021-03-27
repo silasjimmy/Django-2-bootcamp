cca2eaac889a600893377e3ab950d2fb69b98244
Silas Jimmy <jimmysilas17@gmail.com>
2021-03-27 17:19:07 +0300

    Job app up and running

--- PortfolioProject/jobs/models.py
+++ PortfolioProject/jobs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    """
+    Job model object.
+    """
+
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+
+    # def __init__(self, arg):
+    #     super(Job, self).__init__()
+    #     self.arg = arg
